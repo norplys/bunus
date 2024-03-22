73b1f1ade9bbbaffb0f5fd981c05b11294992ce4
norplys <joywinata88@gmail.com>
2024-03-22 11:34:35 +0700

    fix: fixing navbar responsive; and animating it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,19 @@ import { FaShoppingCart } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { useRouter } from "next/navigation";
 import { useCartNotif } from "@/helper/hooks/useCartNotif";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { Menu } from "@headlessui/react";
+import { use, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { FaSpoon } from "react-icons/fa6";
+import { TbGrillFork } from "react-icons/tb";
+import { useEffect } from "react";
 
 export default function Navbar() {
   let token = null;
-  if (typeof window !== "undefined") {
-    token = localStorage.getItem("token") || "";
-  }
+  useEffect(() => {
+    token = localStorage.getItem("token");
+  }, []);
+  const [open, setOpen] = useState(false);
   const { data, isLoading } = useCartNotif(token);
   const { push } = useRouter();
   const { user, setUser } = useUser();
@@ -60,7 +66,7 @@ export default function Navbar() {
                   ""
                 ) : (
                   <div className="rounded-full w-4 h-4 text-center bg-primary-red absolute -right-1 -top-1 text-xs text-white font-bold">
-                    {data}
+                    {data.total}
                   </div>
                 )}
                 <FaShoppingCart className="text-md text-white" />
@@ -77,7 +83,69 @@ export default function Navbar() {
               Login
             </Link>
           )}
-          <GiHamburgerMenu className="text-3xl md:hidden" />
+          <Menu>
+            <Menu.Button onClick={() => setOpen(!open)} className="md:hidden">
+              <div className="relative w-9 h-9">
+                <FaSpoon
+                  className={`text-2xl  top-1 absolute -right-1 ${open ? "rotate-0 right-0" : "-rotate-45"} duration-300`}
+                />
+                <TbGrillFork
+                  className={`text-3xl  absolute top-0 -left-1 ${open ? "rotate-0 left-1" : "rotate-45"} duration-300`}
+                />
+              </div>
+            </Menu.Button>
+            <AnimatePresence>
+              {open && (
+                <Menu.Items
+                  className="absolute right-0 top-16 w-40 bg-white shadow-md rounded-md p-3 flex flex-col gap-2 md:hidden"
+                  static
+                  initial={{ scale: 0, rotate: -360 }}
+                  animate={{ scale: 1, rotate: 0 }}
+                  exit={{ scale: 0, rotate: -360 }}
+                  as={motion.div}
+                >
+                  {user ? (
+                    <div className="flex gap-3 items-center flex-col">
+                      <p className="font-bold text-primary-orange">
+                        Halo, {user.name}
+                      </p>
+                      <Link href={"/menu"}>Menu</Link>
+                      <Link href={"/#testimoni"}>Testimoni</Link>
+                      <button
+                        onClick={logOut}
+                        className="border border-primary-red font-bold text-primary-red px-4 py-1 rounded-md hover:text-white hover:bg-primary-red duration-300"
+                      >
+                        Keluar
+                      </button>
+                      <Link
+                        href={"/cart"}
+                        className="relative bg-primary-cyan rounded-full p-2"
+                      >
+                        {isLoading ? (
+                          ""
+                        ) : (
+                          <div className="rounded-full w-4 h-4 text-center bg-primary-red absolute -right-1 -top-1 text-xs text-white font-bold">
+                            {data.total}
+                          </div>
+                        )}
+                        <FaShoppingCart className="text-md text-white" />
+                      </Link>
+                      <Link href={"/profile"}>
+                        <CgProfile className="text-3xl" />
+                      </Link>
+                    </div>
+                  ) : (
+                    <Link
+                      href={"/login"}
+                      className="border border-primary-cyan font-bold text-primary-cyan px-4 py-1 rounded-md hover:text-white hover:bg-primary-cyan duration-300 text-center"
+                    >
+                      Login
+                    </Link>
+                  )}
+                </Menu.Items>
+              )}
+            </AnimatePresence>
+          </Menu>
         </div>
       </div>
     </div>
