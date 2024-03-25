9565ca1390062095c9d26aee51a04f961cf53491
norplys <joywinata88@gmail.com>
2024-03-25 09:49:54 +0700

    feat: consuming homepage sendmail api

--- src/components/home/ContactForm.tsx
+++ src/components/home/ContactForm.tsx
@@ -2,6 +2,7 @@
 import LeftSlide from "@/helper/animation-framer/LeftSlide";
 import { useForm, SubmitHandler } from "react-hook-form";
 import toast from "react-hot-toast";
+import axios from "axios";
 
 export default function ContactForm() {
   const {
@@ -12,33 +13,58 @@ export default function ContactForm() {
   const onSubmit: SubmitHandler<Record<string, any>> = async (
     data: Record<string, string>,
   ) => {
-    console.log(data);
+    try {
+      const res = axios.post(
+        "https://bunus-be-production.up.railway.app/v1/send-email",
+        data,
+      );
+      await toast.promise(
+        res,
+        {
+          loading: "Mengirim Pesan",
+          success: "Pesan Terkirim",
+          error: "Gagal Mengirim Pesan",
+        },
+        {
+          position: "bottom-left",
+        },
+      );
+    } catch (error) {
+      console.error(error);
+    }
   };
-
   return (
     <form className="" onSubmit={handleSubmit(onSubmit)}>
       <LeftSlide design="flex flex-col gap-5 md:min-w-96" delay={0}>
         <input
-          {...register("name", { required: "Mohon Isi Kolom Ini" })}
+          {...register("name", { required: "Nama Wajib Diisi" })}
           type="text"
           placeholder="Nama"
-          className="border-2 border-primary-orange rounded-xl px-3 py-1 w-full"
+          className={`border-2 rounded-xl px-3 py-1 w-full ${errors.name ? "border-red-500" : "border-primary-orange"} focus:outline-none`}
         />
 
         <input
-          {...register("email", { required: "Mohon Isi Kolom Ini" })}
+          {...register("email", {
+            required: "Email Wajib Diisi",
+            pattern: {
+              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+              message: "Email Tidak Valid",
+            },
+          })}
           type="text"
           placeholder="Email"
-          className="border-2 border-primary-orange rounded-xl px-3 py-1 w-full"
+          className={`border-2 rounded-xl px-3 py-1 w-full ${errors.email ? "border-red-500" : "border-primary-orange"} focus:outline-none`}
         />
 
         <textarea
-          {...register("message", { required: "Mohon Isi Kolom Ini" })}
+          {...register("message", { required: "Pesan Wajib Diisi" })}
           placeholder="Pesan"
-          className="border-2 border-primary-orange rounded-xl px-3 py-1 w-full h-40"
+          className={`border-2 rounded-xl px-3 py-1 w-full h-40 ${errors.message ? "border-red-500" : "border-primary-orange"} focus:outline-none`}
         ></textarea>
-
-        <button className="py-1 px-3 font-bold rounded-xl mb-4 w-fit bg-primary-cyan shadow-xl text-white xl:text-lg scale-95 hover:shadow-2xl hover:scale-100 duration-300 md:text-sm">
+        <button
+          className="py-1 px-3 font-bold rounded-xl mb-4 w-fit bg-primary-cyan shadow-xl text-white xl:text-lg scale-95 hover:shadow-2xl hover:scale-100 duration-300 md:text-sm"
+          disabled={isSubmitting}
+        >
           Kirim Pesan
         </button>
       </LeftSlide>
