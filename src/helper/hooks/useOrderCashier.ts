18c65f02a540743adc9e02e70c48686f41d60b04
norplys <joywinata88@gmail.com>
2024-05-13 10:27:49 +0700

    fix: fixing endpoint for get cashier order

--- src/helper/hooks/useOrderCashier.ts
+++ src/helper/hooks/useOrderCashier.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const fetchOrderAdmin = async (token: string) => {
   const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_BACKEND_LINK}/v1/orders/admin`,
+    `${process.env.NEXT_PUBLIC_BACKEND_LINK}/v1/orders/cashier`,
     {
       headers: {
         Authorization: `Bearer ${token}`,
