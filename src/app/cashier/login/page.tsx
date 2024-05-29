53c9e4fa4e8c3691ceccb0a9e09612b8a58221f2
norplys <joywinata88@gmail.com>
2024-05-29 11:25:58 +0700

    feat: adding login protector

--- src/app/cashier/login/page.tsx
+++ src/app/cashier/login/page.tsx
@@ -2,8 +2,36 @@
 import Link from "next/link";
 import SideCashier from "@/components/cashier/SideCashier";
 import LoginCashier from "@/components/cashier/login/LoginCashier";
+import { useEffect, useState } from "react";
+import Protector from "@/components/Protector";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function CashierLogin() {
+  const [loading, setLoading] = useState(true);
+  const { push } = useRouter();
+  const validateJWT = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_LINK}/v1/validate/admin`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      push("/cashier/dashboard");
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    validateJWT();
+  }, []);
+  if (loading) {
+    return <Protector />;
+  }
   return (
     <section className="flex h-screen items-center justify-center md:flex-col xl:flex-row overflow-hidden">
       <div className="flex-1  bg-white flex justify-center items-center flex-col gap-5 xl:min-h-screen md:p-5 p-2">
