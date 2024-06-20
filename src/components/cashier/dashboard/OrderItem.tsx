8bf04e056ae7f77a32dd8faef8c3b45286f7b10c
norplys <joywinata88@gmail.com>
2024-06-20 19:00:18 +0700

    fix: attempt 14

--- src/components/cashier/dashboard/OrderItem.tsx
+++ src/components/cashier/dashboard/OrderItem.tsx
@@ -161,7 +161,7 @@ export default function OrderItem({
         </button>
         <button
           className="bg-green-600 px-2 py-1 rounded-lg text-white font-bold"
-          onClick={() => handlePrint(printData.current, characteristic)}
+          onClick={() => handlePrint(DATA, characteristic)}
         >
           Print
         </button>
