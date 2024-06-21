e9c11e360718631225647c93becb79e17289c40d
norplys <joywinata88@gmail.com>
2024-06-21 21:44:37 +0700

    fix: fixing PWA

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
 /** @type {import('next').NextConfig} */
-
 module.exports = {
   reactStrictMode: true,
   images: {
