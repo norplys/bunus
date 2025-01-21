b37b1d715bc8f02d7b4bdbed0f2d56963e931d4d
norplys <joywinata88@gmail.com>
2025-01-21 11:39:22 +0700

    fix: fixing deployment

---
+++ src/components/protector.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+export default function Protector() {
+  return (
+    <div className="bg-[url(/pattern.svg)] h-screen grid justify-items-center items-center absolute top-0 left-0 w-screen z-30 bg-cover">
+      <div className="flex justify-center items-center gap-5 animate-pulse">
+        <Image
+          src="/logo.svg"
+          alt="icon"
+          width={300}
+          height={300}
+          className="w-24 h-24 border-8 border-white bg-orange-100 rounded-full shadow-2xl hover:scale-95 hover:shadow-none duration-300 md:w-52 md:h-52 xl:w-80 xl:h-80"
+          priority={true}
+        />
+        <h1 className="xl:text-7xl font-bold text-white md:text-5xl text-xl">
+          BUBUR NUSANTARA
+        </h1>
+      </div>
+    </div>
+  );
+}
